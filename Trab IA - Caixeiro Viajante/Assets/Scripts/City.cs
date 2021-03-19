8c093c7b86d419822e56b50636779b6cce29c3c1
FehMello <72583255+FehMello@users.noreply.github.com>
2021-03-19 12:42:23 -0300

    Project

---
+++ Trab IA - Caixeiro Viajante/Assets/Scripts/City.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class City : MonoBehaviour
+{
+    //Nao colocar isso publico se nao a unity fica considerando o valor do inspector so grr
+    [SerializeField]
+    private int ID; //ID que vai ser usada no cromossomo do individuo rota
+    private bool isOrigin = false; // Diz se essa cidade é a origem, lembrando que a rota tem que sair da origem e chegar na origem.
+    [SerializeField]
+    private TMP_Text IDtext;
+
+    void Start()
+    {
+        IDtext = transform.GetComponentInChildren<TMP_Text>();
+    }
+
+    public void SetID(int value)
+    {
+        ID = value;
+    }
+
+    public int GetID()
+    {
+        Debug.Log("This city ID is " + ID);
+        return ID;
+    }
+
+    public void SetText(int value)
+    {
+        IDtext.SetText(value.ToString());
+        
+    }
+
+
+
+}
---
+++ Trab IA - Caixeiro Viajante/Assets/Scripts/Manager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class Manager : MonoBehaviour
+{
+    //nossa população/individuos q estamos testando n sao as cidades e sim as rotas
+    //as rotas (individuos) terao a cidade no array.
+
+    public int qntCidades;//Objetos que os individuos vão 'interagir' para achar a melhor rota
+    
+    public GameObject CityObj;
+
+    private List <Rota> _Rotas = new List<Rota>(); //O tamanho dessa lista tem que se manter igual à qnt de população durante o programa todo.
+    [SerializeField]
+    private List<GameObject> _Cidades = new List<GameObject>();//Pra ter controle das cidades
+    [SerializeField]
+    private int _Populacao = 0;// Numero de individuos a serem criados, esse numero deve se manter fixo.
+
+    void Start()
+    {
+        //Cria cidades
+        for (int i = 0; i < qntCidades; i++)
+        {
+            GameObject cityTMP;
+            Vector3 position = new Vector3(Random.Range(-13.0F, 13.0F), 0, Random.Range(-13.0F, 13.0F));
+            cityTMP=Instantiate(CityObj, position, Quaternion.identity) as GameObject;
+            _Cidades.Add(cityTMP);
+        }
+
+        //int temp = 0;
+
+        //Da ids pras cidades.
+        for (int i = 0; i < qntCidades; i++)
+        {
+            City cidadetemp;
+            cidadetemp = _Cidades[i].GetComponent<City>();
+            cidadetemp.SetID(i);
+            cidadetemp.SetText(cidadetemp.GetID());
+            //_Cidades[i].GetComponent<City>().SetID(i);
+            //temp=_Cidades[i].GetComponent<City>().GetID();
+            //_Cidades[i].GetComponent<City>().SetID(i);
+
+        }
+
+        //Pega numero de cidades 
+        _Populacao = qntCidades;
+        
+        
+    }
+
+   
+    void Update()
+    {
+        
+
+
+    }
+}
